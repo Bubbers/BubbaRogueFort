52256d1c4abef89d6ed8910996a44441c43c5dea
Viktor Sjölind <viktor@sjolind.se>
2016-06-21 09:30:36 +0200

    Indentation

--- src/main.cpp
+++ src/main.cpp
@@ -21,28 +21,28 @@ GameObject *player;
 Scene scene;
 
 void loadMeshes() {
-  /* Shader setup done once for all meshes that use it */
-  ShaderProgram* standardShader = ResourceManager::getShader(SIMPLE_SHADER_NAME);
+    /* Shader setup done once for all meshes that use it */
+    ShaderProgram* standardShader = ResourceManager::getShader(SIMPLE_SHADER_NAME);
 
-  /* Load player mesh and attach it to the player GameObject */
-  Mesh *playerMesh = ResourceManager::loadAndFetchMesh("../meshes/player.fbx");
-                                            // references are from the the build folder
+    /* Load player mesh and attach it to the player GameObject */
+    Mesh *playerMesh = ResourceManager::loadAndFetchMesh("../meshes/player.fbx");
+                                              // references are from the the build folder
 
-  player = new GameObject(playerMesh);
-  player->setLocation(make_vector(0.0f, 0.0f, 0.0f));
-  StandardRenderer* stdrenderer = new StandardRenderer(playerMesh, player, standardShader);
-  player->addRenderComponent(stdrenderer);
-  player->setDynamic(true);
+    player = new GameObject(playerMesh);
+    player->setLocation(make_vector(0.0f, 0.0f, 0.0f));
+    StandardRenderer* stdrenderer = new StandardRenderer(playerMesh, player, standardShader);
+    player->addRenderComponent(stdrenderer);
+    player->setDynamic(true);
 
     camera = new PlayerCamera(
-               make_vector(10.0f, 10.0f, 10.0f),
-               make_vector(0.0f, 0.0f, 0.0f),
-               UP_VECTOR, 45, float(SCREEN_WIDTH) / float(SCREEN_HEIGHT),
-               0.1f, 50000.0f);
+                 make_vector(10.0f, 10.0f, 10.0f),
+                 make_vector(0.0f, 0.0f, 0.0f),
+                 UP_VECTOR, 45, float(SCREEN_WIDTH) / float(SCREEN_HEIGHT),
+                 0.1f, 50000.0f);
 
-  player->addComponent(camera);
-  /* Add the player to the scene */
-  scene.addShadowCaster(player);
+    player->addComponent(camera);
+    /* Add the player to the scene */
+    scene.addShadowCaster(player);
 }
 
 void idle(float timeSinceStart,float timeSinceLastCall) {
@@ -56,26 +56,26 @@ void idle(float timeSinceStart,float timeSinceLastCall) {
 }
 
 void display(float timeSinceStart,float timeSinceLastCall) {
-  renderer->drawScene(camera, &scene, timeSinceStart);
+    renderer->drawScene(camera, &scene, timeSinceStart);
 }
 
 void resize(int newWidth, int newHeight) {
-  renderer->resize(newWidth, newHeight);
+    renderer->resize(newWidth, newHeight);
 }
 
 int main(int argc, char *argv[]) {
-  Window* win = new Window(SCREEN_WIDTH, SCREEN_HEIGHT, "Bubba Rogue Fort");
+    Window* win = new Window(SCREEN_WIDTH, SCREEN_HEIGHT, "Bubba Rogue Fort");
 
-  win->setResizeMethod(resize);
-  win->setIdleMethod(idle);
-  win->setDisplayMethod(display);
+    win->setResizeMethod(resize);
+    win->setIdleMethod(idle);
+    win->setDisplayMethod(display);
 
-  renderer = new Renderer();
-  renderer->initRenderer(SCREEN_WIDTH, SCREEN_HEIGHT);
+    renderer = new Renderer();
+    renderer->initRenderer(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-  loadMeshes();
+    loadMeshes();
 
-  win->start(60);
-  return 0;
+    win->start(60);
+    return 0;
 }
 
