9b6c166393e51a1431a4eae79c7e666b9f034127
Simon Lindhén <simon.lindhen@hotmail.com>
2016-10-01 18:17:19 +0200

    Tidied up some if-statements

--- src/components/MoveComponentWithCollision.cpp
+++ src/components/MoveComponentWithCollision.cpp
@@ -43,52 +43,20 @@ void MoveComponentWithCollision::checkKeyboardKeys() {
     ControlStatus   cs_H = cm->getStatus(MOVE_H);
     ControlStatus   cs_V = cm->getStatus(MOVE_V);
 
-    if(cs_H.isActive() && cs_V.isActive())
-    {
-        if(cs_H.getValue() > 0 && cs_V.getValue() < 0)
-        {
-            this->setVelocity(make_vector(0.01f, 0.00f, 0.01f) / SQRT_2);
-            owner->setRotation(make_quaternion_axis_angle(UP_VECTOR, M_PI / 4));
-        }
-        else if(cs_H.getValue() > 0 && cs_V.getValue() > 0)
-        {
-            this->setVelocity(make_vector(0.01f, 0.00f, -0.01f) / SQRT_2);
-            owner->setRotation(make_quaternion_axis_angle(UP_VECTOR, (3 * M_PI) / 4));
-        }
-        else if(cs_H.getValue() < 0 && cs_V.getValue() < 0)
-        {
-            this->setVelocity(make_vector(-0.01f, 0.0f, 0.01f) / SQRT_2);
-            owner->setRotation(make_quaternion_axis_angle(UP_VECTOR, M_PI / -4));
-        }
-        else{
-            this->setVelocity(make_vector(-0.01f, 0.0f, -0.01f) / SQRT_2);
-            owner->setRotation(make_quaternion_axis_angle(UP_VECTOR, (-3 * M_PI) / 4));
-        }
-    }
-    else if(cs_H.isActive())
-    {
-        if(cs_H.getValue() > 0)
-        {
-            this->setVelocity(make_vector(0.01f, 0.0f, 0.0f));
-            owner->setRotation(make_quaternion_axis_angle(UP_VECTOR, M_PI / 2));
-        }
-        else{
-            this->setVelocity(make_vector(-0.01f, 0.0f, 0.0f));
-            owner->setRotation(make_quaternion_axis_angle(UP_VECTOR, M_PI / -2));
-        }
-    }
-    else if(cs_V.isActive())
-    {
-        if(cs_V.getValue() < 0) {
-            this->setVelocity(make_vector(0.0f, 0.0f, 0.01f));
-            owner->setRotation(make_quaternion_axis_angle(UP_VECTOR, M_PI * -2));
-        }
-        else{
-            this->setVelocity(make_vector(0.0f, -0.0f, -0.01f));
-            owner->setRotation(make_quaternion_axis_angle(UP_VECTOR, M_PI / -1.0f));
-        }
-    }
-    else if(!cs_H.isActive() && !cs_V.isActive()){
+    if(cs_H.isActive() || cs_V.isActive()) {
+        float x = cs_H.getValue() / 10000.0f;
+        float y = cs_V.getValue() / 10000.0f;
+        float3 vec = make_vector(x, 0.0f, -y);
+        if(cs_H.isActive() && cs_V.isActive())
+            vec = vec/SQRT_2;
+        this->setVelocity(vec);
+        float angle = y == 0.0f ? M_PI_2 : atan(abs(x/y));
+        if(y > 0.0f)
+            angle = M_PI - angle;
+        if(x < 0.0f)
+            angle = -angle;
+        owner->setRotation(make_quaternion_axis_angle(UP_VECTOR,angle));
+    } else {
         this->setVelocity(make_vector(0.0f, 0.0f, 0.0f));
     }
 }
