02762f0b3f396774936b79d37e330ea3d1a5767e
Simon Lindhén <simon.lindhen@hotmail.com>
2016-10-02 13:12:12 +0200

    Added has function for bandit

--- src/logic/Bandit.h
+++ src/logic/Bandit.h
@@ -16,13 +16,26 @@ public:
     std::unordered_set<std::string> getAttacks();
     AttackResult performAttack(std::string);
     void takeDamage(AttackResult damage);
-    std::string getName();
+    std::string getName() const;
     virtual void update(float dt) override {};
     virtual bool isAlive();
-    virtual int getHealth();
+    virtual int getHealth() const;
     virtual bool equals(Bandit* other);
     virtual void addDamageListener(std::function<void (int)> listener);
     virtual int getMaxHealth();
+
+    /**
+     * Hash function for Bandit
+     */
+    struct BanditHash
+    {
+        std::size_t operator()(Bandit* const& bandit) const
+        {
+            std::size_t h1 = std::hash<std::string>()(bandit->getName());
+            std::size_t h2 = std::hash<int>()(bandit->getHealth());
+            return h1 ^ (h2 << 1); // or use boost::hash_combine
+        }
+    };
 
 protected:
     Bandit(std::string name, int maxHealth, Stats* stats, std::unordered_map<std::string,Attack>* attacks);
