1eb07363d9ca1c76bb9e03a85861afad05156027
Viktor Sjölind <viktor@sjolind.se>
2016-10-02 14:00:17 +0200

    Fixed warnings in CrewMember and Dog

--- src/logic/CrewMember.cpp
+++ src/logic/CrewMember.cpp
@@ -1,13 +1,12 @@
-//
-// Created by simon on 2016-10-01.
-//
-
 #include "CrewMember.h"
+#include "Stats.h"
 
-CrewMember::CrewMember(std::string name, int maxHealth, CrewType crewType) : crewType(crewType), Bandit(name, maxHealth) {}
+CrewMember::CrewMember(std::string name, int maxHealth, CrewType crewType):
+    crewType(crewType), Bandit(name, maxHealth) {}
 
-CrewMember::CrewMember(std::string name, int maxHealth, CrewType crewType, Stats *stats, std::unordered_map<string, Attack>* attacks)
-        : crewType(crewType), Bandit(name, maxHealth, stats, attacks) {}
+CrewMember::CrewMember(std::string name, int maxHealth, CrewType crewType,
+    Stats *stats, std::unordered_map<std::string, Attack>* attacks)
+  : crewType(crewType), Bandit(name, maxHealth, stats, attacks) {}
 
 CrewMember::CrewType CrewMember::getCrewType() {
     return crewType;
